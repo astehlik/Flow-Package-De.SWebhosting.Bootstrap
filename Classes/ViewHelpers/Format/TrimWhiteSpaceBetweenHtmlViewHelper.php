39b268c9d2efb50c33f9a24a48dbd7e19b87c867
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-12-16 00:27:49 +0100

    [FEATURE] Trim whitespace between HTML tags view helper

---
+++ Classes/ViewHelpers/Format/TrimWhiteSpaceBetweenHtmlViewHelper.php
@@ -0,0 +1,32 @@
+<?php
+namespace De\SWebhosting\Bootstrap\ViewHelpers\Format;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package                          *
+ * "De.SWebhosting.Bootstrap".                                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * This view helper removes the whitespace between all HTML tags
+ * that are rendered in its children.
+ */
+class TrimWhiteSpaceBetweenHtmlViewHelper extends AbstractViewHelper {
+
+	/**
+	 * Removes whitespace in the rendered child HTML using a regular expression.
+	 *
+	 * @return string
+	 */
+	public function render() {
+		$result = $this->renderChildren();
+		return preg_replace('~>\s+<~', '><', $result);
+	}
+}
